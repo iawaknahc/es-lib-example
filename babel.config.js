fb248a3e4eb35a0f7e47392eff9f69898a80bd65
Louis Chan <louischan0325@gmail.com>
2018-09-12 00:06:27 +0800

    Do not use transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,10 @@
-// Use all presets except @babel/preset-stage-x and @babel/preset-minify.
-// @babel/preset-stage-x are going to be deprecated.
-// @babel/preset-minify is supposed to be used by application.
 const presets = [
-  {
-    presets: [
-      [
-        "@babel/preset-env",
-        {
-          // Do not transform module syntax.
-          // It is because module syntax is bound to
-          // bundle output format, which is the job of the bundler.
-          modules: false,
-        },
-      ],
-    ],
-    plugins: [
-      [
-        "@babel/plugin-transform-runtime",
-        {
-          corejs: 2,
-          helpers: true,
-          regenerator: true,
-          useESModules: true,
-        },
-      ],
-    ],
-  },
+  [
+    "@babel/preset-env",
+    {
+      modules: false,
+    },
+  ],
   "@babel/preset-react",
   {
     plugins: [
