909950fce67793b605f962cb579d5314aa412244
Louis Chan <louischan0325@gmail.com>
2019-03-10 19:49:24 +0800

    Use Babel to compile TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(j|t)sx?$",
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,25 @@
-import typescript from "rollup-plugin-typescript";
+import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
 import resolve from "rollup-plugin-node-resolve";
-import pkg from "./package.json";
-
-const deps = Object.keys(pkg.dependencies || {});
-const peerDeps = Object.keys(pkg.peerDependencies || {});
-const allDeps = deps.concat(peerDeps);
 
 const extensions = [".mjs", ".js", ".jsx", ".ts", ".tsx"];
 
 const plugins = [
-  resolve({
+  babel({
     extensions,
+    exclude: "node_modules/**",
   }),
   json({
     preferConst: true,
     indent: "  ",
   }),
+  resolve({
+    extensions,
+  }),
   commonjs({
     include: "node_modules/**",
   }),
-  typescript(),
 ];
 
 const output = [
@@ -35,21 +33,8 @@ const output = [
   },
 ];
 
-function external(id) {
-  if (id === "tslib") {
-    return false;
-  }
-  for (const d of allDeps) {
-    if (id.startsWith(d)) {
-      return true;
-    }
-  }
-  return false;
-}
-
 export default {
   input: "src/index.ts",
-  external,
   plugins,
   output,
 };
